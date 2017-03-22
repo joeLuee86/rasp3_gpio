5d47d93af085681ef95fc85bd743f58c3d21a486
Joe Liu <joe.luee@outlook.com>
2017-03-22 22:17:48 +0800

    add channel handler

--- blink_gpio.py
+++ blink_gpio.py
@@ -7,15 +7,37 @@ print "hello, world!"
 import time
 import RPi.GPIO as GPIO 
 
+CHANNEL_1 = 7 		# Set GPIO_7 as channel_1 pump
+
+HUMIDITY_SENSOR = 8 # Set GPIO_8 as humidity sensor
+
+PUMP_OPEN = 1 		# start the pump
+PUMP_CLOSE = 0 		# stop the pump
+
+# initialize the GPIO for pump control
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(CHANNEL_1, GPIO.OUT)
+GPIO.setup(HUMIDITY_SENSOR, GPIO.IN)
 
-GPIO.setup(7, GPIO.OUT)
+# channel handle function
+def handleChannelOpen(channel):
+	GPIO.output(channel, PUMP_OPEN)
 
-# loop through 50 times, on/off for 1 second
-for i in range(50):
-	GPIO.output(7, True)
-	time.sleep(1)
-	GPIO.output(7, False)
-	time.sleep(1)
+def handleChannelClose(channel):
+	GPIO.output(channel, PUMP_CLOSE)
 
+# read humidity sensor info
+def readHumiditySensor():
+	return GPIO.input(HUMIDITY_SENSOR)
+
+# start pump procedure
+if (readHumiditySensor() == 1):
+	sys.exit(0)   	# we don't to washer the flower, it's still humid
+else:
+	while (readHumiditySensor() == 0):
+		handleChannelOpen(CHANNEL_1)
+		time.sleep(5)
+	handleChannelClose(CHANNEL_1)
+
+# release the GPIO
 GPIO.cleanup
