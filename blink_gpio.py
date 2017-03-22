12bcbc6c4e79c20a2c6531876ba591f36919cd26
Joe Liu <joe.luee@outlook.com>
2017-03-22 22:41:12 +0800

    update blink_gpio.py

--- blink_gpio.py
+++ blink_gpio.py
@@ -4,15 +4,17 @@
 print "hello, world!"
 
 # import the necessary modules
+import sys
+import threading
 import time
 import RPi.GPIO as GPIO 
 
-CHANNEL_1 = 7 		# Set GPIO_7 as channel_1 pump
+CHANNEL_1 = 26 			# Set GPIO_26 as channel_1 pump
 
-HUMIDITY_SENSOR = 8 # Set GPIO_8 as humidity sensor
+HUMIDITY_SENSOR = 21 	# Set GPIO_21 as humidity sensor
 
-PUMP_OPEN = 1 		# start the pump
-PUMP_CLOSE = 0 		# stop the pump
+PUMP_OPEN = 1 			# start the pump
+PUMP_CLOSE = 0 			# stop the pump
 
 # initialize the GPIO for pump control
 GPIO.setmode(GPIO.BOARD)
@@ -30,14 +32,29 @@ def handleChannelClose(channel):
 def readHumiditySensor():
 	return GPIO.input(HUMIDITY_SENSOR)
 
-# start pump procedure
-if (readHumiditySensor() == 1):
-	sys.exit(0)   	# we don't to washer the flower, it's still humid
-else:
-	while (readHumiditySensor() == 0):
-		handleChannelOpen(CHANNEL_1)
-		time.sleep(5)
+def forceClosePump():
 	handleChannelClose(CHANNEL_1)
+	sys.exit(1)
+
+def exitProgram():
+	# release the GPIO
+	GPIO.cleanup
+	sys.exit(0)
+
+# start pump procedure
+if __name__ == "__main__":
+	# force stop the pump after 10 seconds
+	timer = threading.Timer(10, forceClosePump)
+	timer.start
+
+	if (readHumiditySensor() == 1):
+		exitProgram()   	# we don't to washer the flower, it's still humid
+	else:
+		while (readHumiditySensor() == 0):
+			handleChannelOpen(CHANNEL_1)
+			time.sleep(5)
+
+		handleChannelClose(CHANNEL_1)
 
 # release the GPIO
 GPIO.cleanup
