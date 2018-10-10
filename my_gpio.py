378c3bd06b4f1dc8736749da36dc9f8db7eea650
Joe <joe.luee@outlook.com>
2018-10-10 22:16:33 +0800

    code change

--- my_gpio.py
+++ my_gpio.py
@@ -50,7 +50,7 @@ class MyGpio:
 
 	# BCM pins on Raspberry Pi 3B board
 	# BCM_PINS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
-	BCM_PINS = [0, 1, 2, 3, 4, 5, 6]
+	BCM_PINS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 	ONE_MINITE_TICK = 60
 	ONE_HOUR_TICK = 60 * ONE_MINITE_TICK 
